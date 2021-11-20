cfe5a7903e88edc83a0119283a568d73152e0a56
Coding man <qaz3575805@gmail.com>
2021-11-20 18:48:40 +0900

    Initial commit

---
+++ src/main/java/com/study/whau/WhauApplication.java
@@ -0,0 +1,13 @@
+package com.study.whau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhauApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/whau/WhauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.whau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
