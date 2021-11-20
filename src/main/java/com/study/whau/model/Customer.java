2ec2d4379258149974e8b6ad411c680e595401fa
Code-report <qaz3575805@gmail.com>
2021-11-20 19:50:39 +0900

    Yaml default / local 분리

---
+++ src/main/java/com/study/whau/model/Customer.java
@@ -0,0 +1,17 @@
+package com.study.whau.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Customer {
+    @Id
+    private String email;
+
+    private String name;
+
+    private String password;
+}
